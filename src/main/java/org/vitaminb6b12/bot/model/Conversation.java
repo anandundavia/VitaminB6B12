61f805f471aa9519c038c268252522e7dec9477b
Anand Undavia <abundavia@gmail.com>
2017-07-21 21:02:52 +0530

    Persistence Layer

---
+++ src/main/java/org/vitaminb6b12/bot/model/Conversation.java
@@ -0,0 +1,35 @@
+package org.vitaminb6b12.bot.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import ai.api.model.AIResponse;
+
+public class Conversation {
+	@Id
+	@Indexed
+	private final Long id;
+	private AIResponse data;
+
+	public Conversation() {
+		this.id = System.currentTimeMillis();
+	}
+
+	public Conversation(AIResponse reply) {
+		this.id = System.currentTimeMillis();
+		data = reply;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public AIResponse getData() {
+		return data;
+	}
+
+	public void setData(AIResponse data) {
+		this.data = data;
+	}
+
+}
---
+++ src/main/java/org/vitaminb6b12/bot/persistence/ConversationRepository.java
@@ -0,0 +1,10 @@
+package org.vitaminb6b12.bot.persistence;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.vitaminb6b12.bot.model.Conversation;
+
+@Repository
+public interface ConversationRepository extends MongoRepository<Conversation, Long> {
+
+}
